a8428ed6b8f18a446b93c97f20acd39e78c570ce
xFra96 <f.spagnoli1996@gmail.com>
2021-11-27 16:12:06 +0100

    Aggiunto text to speech - primo test con quello integrato

--- src/components/phrase.js
+++ src/components/phrase.js
@@ -2,11 +2,43 @@ import { useAppContext } from '../misc/model.provider'
 
 const Phrase = () => {
     const { current_word } = useAppContext()
-
+    const synth = window.speechSynthesis;
+    const voices = synth.getVoices()
+    const speak = () => {
+        let utterThis = new SpeechSynthesisUtterance(current_word);
+        let selectedOption = document.querySelector("#selectlingua").selectedOptions[0].getAttribute('value');
+        for (var i = 0; i < voices.length; i++) {
+            if (voices[i].name === selectedOption) {
+                utterThis.voice = voices[i];
+            }
+        }
+        utterThis.pitch = 1;
+        utterThis.rate = 0.8;
+        synth.speak(utterThis);
+    }
     return (
-        <div className="textcontainer">
-            <p>{current_word}</p>
-        </div>
+        <>
+            <div className="textcontainer">
+                <p>{current_word}</p>
+            </div>
+            <div className="row">
+                <div className="col-12 mb-4">
+                    <p>Scegli una voce / Choose Language</p>
+                    <select className="w-50 mx-auto form-select" id="selectlingua" aria-label="Seleziona lingua">
+                        {voices.map(el => {
+                            return (
+                                <option value={el.name}>{el.name}</option>
+                            )
+                        }
+                        )
+                        }
+                    </select>
+                </div>
+                <div className="col-12">
+                    <button className="w-50 mx-auto btn btn-primary" onClick={() => { speak() }}>Parla</button>
+                </div>
+            </div>
+        </>
     )
 }
 
